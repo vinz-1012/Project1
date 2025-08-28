7b2a1ea5d687f0f1b32b8711abfb2be2cf1abc49
Vĩnh <tanvinh101206@gmail.com>
2025-08-28 10:03:03 +0700

    sửa lần 2

--- Project1/datghe.cpp
+++ Project1/datghe.cpp
@@ -3,6 +3,9 @@ using namespace std;
 
 int main() {
     char ghe[5][5];
+    map<pair<int, int>, string> maVe;
+    int soVe = 0;
+
     for (int i = 0; i < 5; i++)
         for (int j = 0; j < 5; j++)
             ghe[i][j] = 'O';
@@ -19,28 +22,54 @@ int main() {
         cout << "1. Dat ghe\n2. Huy ghe\n3. Thoat\nChon: ";
         cin >> chon;
 
-        if (chon == 1 || chon == 2) {
+        if (chon == 1) {
+            int n;
+            cout << "Ban muon dat bao nhieu ghe? ";
+            cin >> n;
+
+            for (int k = 0; k < n; k++) {
+                int hang, cot;
+                cout << "\nDat ghe thu " << k + 1 << ":\n";
+                cout << "Nhap hang (1-5): "; cin >> hang;
+                cout << "Nhap cot (1-5): "; cin >> cot;
+                hang--; cot--;
+
+                if (hang < 0 || hang >= 5 || cot < 0 || cot >= 5) {
+                    cout << "Vi tri khong hop le!\n";
+                }
+                else if (ghe[hang][cot] == 'X') {
+                    cout << "Ghe da duoc dat!\n";
+                }
+                else {
+                    ghe[hang][cot] = 'X';
+                    soVe++;
+                    string id = "VE" + to_string(1000 + soVe);
+                    maVe[{hang, cot}] = id;
+                    cout << "Dat thanh cong! Ma ve cua ban: " << id << endl;
+                }
+            }
+        }
+        else if (chon == 2) {
             int hang, cot;
-            cout << "Nhap hang (1-5): ";
-            cin >> hang;
-            cout << "Nhap cot (1-5): ";
-            cin >> cot;
+            cout << "Nhap hang (1-5): "; cin >> hang;
+            cout << "Nhap cot (1-5): "; cin >> cot;
             hang--; cot--;
 
             if (hang < 0 || hang >= 5 || cot < 0 || cot >= 5) {
                 cout << "Vi tri khong hop le!\n";
             }
-            else if (chon == 1) {
-                if (ghe[hang][cot] == 'X') cout << "Ghe da duoc dat!\n";
-                else { ghe[hang][cot] = 'X'; cout << "Dat thanh cong!\n"; }
+            else if (ghe[hang][cot] == 'O') {
+                cout << "Ghe dang trong!\n";
             }
             else {
-                if (ghe[hang][cot] == 'O') cout << "Ghe dang trong!\n";
-                else { ghe[hang][cot] = 'O'; cout << "Huy thanh cong!\n"; }
+                cout << "Huy thanh cong! Ma ve da huy: " << maVe[{hang, cot}] << endl;
+                ghe[hang][cot] = 'O';
+                maVe.erase({ hang, cot });
             }
         }
     }
 
     return 0;
 }
+
 
